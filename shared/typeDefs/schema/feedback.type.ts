0faba8f26e62dbfae8c6fc40c52afb89ecabb72a
Hungpq2810 <hung.pq184270@sis.hust.edu.vn>
2023-12-18 22:33:01 +0700

    thêm màn quản lý cho admin, sửa trang home

--- shared/typeDefs/schema/feedback.type.ts
+++ shared/typeDefs/schema/feedback.type.ts
@@ -1,9 +1,13 @@
+export interface IFeedBacks {
+  feedbacks: IFeedback[];
+}
+
 export interface IFeedback {
-    id: number;
-    user_id: number;
-    activity_id: number;
-    title: string;
-    content: string;
-    created_at: string;
-    updated_at: string;
+  id: number;
+  user_id: number;
+  activity_id: number;
+  title: string;
+  content: string;
+  created_at?: any;
+  updated_at?: any;
 }
